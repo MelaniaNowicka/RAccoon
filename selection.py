f714a6ae502776926094673c68e9b40c8dcfa3da
Melania Nowicka <melania.nowicka@gmail.com>
2020-08-06 18:06:23 +0200

    Fix bug in selection

--- selection.py
+++ selection.py
@@ -1,17 +1,19 @@
 import random
+import eval
 
 random.seed(1)
+
 
 # one parent selection function
 def select_parent(population, tournament_size, first_parent_id):
 
-    #create empty tournament
+    # create empty tournament
     tournament = []
 
     # drawing parents from a population without replacement
     for i in range(0, int(tournament_size*len(population))):
         candidate = random.randrange(0, len(population))  # randomly choose a candidate
-        while population[candidate] == first_parent_id:  # check if ids of parent are unique
+        while candidate == first_parent_id:  # check if ids of parent are unique
             candidate = random.randrange(0, len(population))
         else:
             tournament.append(candidate)  # add candidate to the tournament
@@ -19,7 +21,7 @@ def select_parent(population, tournament_size, first_parent_id):
     # choosing the best parent for crossover
     best_candidate = tournament[0]  # assign first in tournament as the best candidate
     for candidate in tournament:  # compare the best candidate to others
-        if population[candidate].score > population[best_candidate].score:  # if new candidate is better
+        if eval.is_higher(population[best_candidate].score, population[candidate].score):
             best_candidate = candidate  # assign to the best candidate
 
     return best_candidate
@@ -28,9 +30,9 @@ def select_parent(population, tournament_size, first_parent_id):
 # tournament selection of parents for crossover
 def select(population, tournament_size):
 
-    # select first parent (parents must have different ids!)
+    # select first parent, parents must have different ids (-1 - no id is blocked)
     first_parent_id = select_parent(population, tournament_size, -1)
-    # select second parent (parents must have different ids!)
+    # select second parent (parents must have different ids)
     second_parent_id = select_parent(population, tournament_size, first_parent_id)
 
     return first_parent_id, second_parent_id
