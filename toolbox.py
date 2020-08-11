cd7c413100356a1f86ea8f310b7c93b62f2f6a40
Melania Nowicka <melania.nowicka@gmail.com>
2020-08-11 12:50:43 +0200

    Toolbox cleanup

--- toolbox.py
+++ toolbox.py
@@ -1,6 +1,5 @@
-from decimal import Decimal, ROUND_HALF_UP
-import sys
 import preproc
+import tuner
 import pandas
 import random
 
@@ -24,44 +23,10 @@ def compare_folds(fold1, fold2):
     print("DATA MATCH: ", data1.equals(data2))
 
 
-# removal of irrelevant (non-regulated) miRNAs (filled with only 0/1).
-def remove_irrelevant_mirna(dataset_filename):
-
-    dataset, negatives, positives = preproc.read_data(dataset_filename)
-
-    relevant_mirna = []
-    irrelevant_mirna = []
-
-    # sum of miRNA levels (0/1) in each column
-    column_sum = dataset.sum(axis=0, skipna=True)
-
-    dataset_out_filename = dataset_filename.replace(".csv", "_filtered.csv")
-
-    # if miRNA levels sum up to 0 or the number of samples in the dataset - miRNA is irrelevant (non-regulated)
-    # (in other words: the whole column is filled in with 0s or 1s)
-    for id, sum in column_sum.items():
-        if id not in ["ID", "Annots"]:
-            print("S", sum)
-            print(len(dataset.index))
-            if sum == 0 or sum == len(dataset.index):
-
-                irrelevant_mirna.append(id)
-            else:
-                relevant_mirna.append(id)
-
-    # removing irrelevant miRNAs from the dataset
-    dataset = dataset.drop(irrelevant_mirna, axis=1)
-
-    # creating log message
-    print("Number of relevant miRNAs according to a given threshold: " + str(len(relevant_mirna)))
-    print("Number of irrelevant miRNAs according to a given threshold: " + str(len(irrelevant_mirna)))
-
-    dataset.to_csv(dataset_out_filename, sep=";", index=False)
-
-
+# divide data into train and test
 def divide_into_train_test(dataset_filename, train_frac):
 
-    dataset, negatives, positives = preproc.read_data(dataset_filename)
+    dataset, annotation, negatives, positives, features = preproc.read_data(dataset_filename)
     header = dataset.columns.values.tolist()
 
     data_size = len(dataset.index)
@@ -72,6 +37,9 @@ def divide_into_train_test(dataset_filename, train_frac):
     # create training set
     negative_samples_to_draw = int(round(negatives*train_frac/100))
     positive_samples_to_draw = int(round(positives*train_frac/100))
+
+    print("TRAINING DATA SET: ", positive_samples_to_draw+negative_samples_to_draw, " samples")
+    print("TESTING DATA SET: ", data_size - (positive_samples_to_draw + negative_samples_to_draw), " samples")
 
     # draw positive training samples
     training_positives = positive_samples.sample(n=positive_samples_to_draw)  # draw n samples
@@ -88,41 +56,35 @@ def divide_into_train_test(dataset_filename, train_frac):
     training_data = training_negatives.append(training_positives)
     testing_data = testing_negatives.append(testing_positives)
 
-    new_name = "_train_" + str(train_frac) + ".csv"
-    filename = dataset_filename.replace(".csv", new_name)
-    training_data.to_csv(filename, sep=";", index=False)
-
-    new_name = "_test_" + str(100-train_frac) + ".csv"
-    filename = dataset_filename.replace(".csv", new_name)
-    testing_data.to_csv(filename, sep=";", index=False)
-
     return training_data, testing_data
 
 
-def crossvalidation(dataset_filename, kfolds):
+# division of the data set into kfolds
+def divide_into_cv_folds(dataset, kfolds):
 
-    dataset, negatives, positives = preproc.read_data(dataset_filename)
+    samples = len(dataset.index)
+    negatives = dataset[dataset["Annots"] == 0].count()["Annots"]
+    positives = samples - negatives
+
     header = dataset.columns.values.tolist()
-    data_size = len(dataset.index)
 
-    negative_samples = dataset.iloc[:negatives].copy()
-    positive_samples = dataset.iloc[negatives:data_size].copy()
+    negative_samples = dataset.iloc[:negatives].copy()  # copy negative samples
+    positive_samples = dataset.iloc[negatives:samples].copy()  # copy positive samples
 
-    negative_samples_to_draw = int(round(negatives/kfolds))
+    negative_samples_to_draw = int(round(negatives/kfolds))  # number of neg samples to draw
+    positive_samples_to_draw = int(round(positives/kfolds))  # number of pos samples to draw
 
-    positive_samples_to_draw = int(round(positives/kfolds))
+    negative_folds = []  # list of negative folds
+    positive_folds = []  # list of positive folds
 
-    negative_folds = []
-    positive_folds = []
+    train_datasets = []  # list of train folds
+    test_datasets = []   # list of test folds
 
-    train_datasets = []
-    test_datasets = []
+    for fold in range(1, kfolds):  # k-1 times draw positive and negative samples
 
-    for fold in range(1, kfolds):
-
-        data_fold = positive_samples.sample(n=positive_samples_to_draw)
-        positive_folds.append(data_fold.sort_index())
-        ids_to_drop = data_fold.index.values
+        data_fold = positive_samples.sample(n=positive_samples_to_draw)  # draw n samples
+        positive_folds.append(data_fold.sort_index())  # add fold to positive folds
+        ids_to_drop = data_fold.index.values  # drop drawn ids
         positive_samples.drop(ids_to_drop, inplace=True)
 
         data_fold = negative_samples.sample(n=negative_samples_to_draw)
@@ -130,11 +92,12 @@ def crossvalidation(dataset_filename, kfolds):
         ids_to_drop = data_fold.index.values
         negative_samples.drop(ids_to_drop, inplace=True)
 
-    positive_folds.append(positive_samples)
+    positive_folds.append(positive_samples)  # add remaining samples as last folds
     negative_folds.append(negative_samples)
+
     test_folds = []
 
-    for fold in range(0, kfolds):
+    for fold in range(0, kfolds):  # create k test folds
         test_fold = negative_folds[fold].append(positive_folds[fold])
         test_folds.append(test_fold)
         print("TEST FOLD ", fold+1, len(test_fold.index.values))
@@ -145,10 +108,9 @@ def crossvalidation(dataset_filename, kfolds):
                     if not row.equals(fold_row):
                         print("WRONG ROW")
 
-    for fold in range(0, kfolds):
-        new_name = "_test_" + str(fold+1) + ".csv"
-        filename = dataset_filename.replace(".csv", new_name)
-        test_folds[fold].to_csv(filename, sep=";", index=False)
+        test_datasets.append(test_fold)
+
+    for fold in range(0, kfolds):  # create train folds
 
         train_folds_to_merge = []
         for i in range(0, kfolds):
@@ -167,200 +129,7 @@ def crossvalidation(dataset_filename, kfolds):
 
         print("TRAIN FOLD ", fold + 1, len(train_fold.index.values))
         train_fold = train_fold.sort_index()
-        new_name = "_train_" + str(fold + 1) + ".csv"
-        filename = dataset_filename.replace(".csv", new_name)
-        train_fold.to_csv(filename, sep=";", index=False)
 
         train_datasets.append(train_fold)
-        test_datasets.append(test_folds)
 
     return train_datasets, test_datasets
-
-
-# balanced accuracy score
-def calculate_balanced_accuracy(tp, tn, p, n):
-
-    try:
-        balanced_accuracy = (tp/p + tn/n)/2
-    except ZeroDivisionError:
-        print("Error: balanced accuracy - division by zero! No negatives or positives in the dataset!")
-        sys.exit(0)
-
-    return balanced_accuracy
-
-
-# evaluation of the population
-def evaluate_classifier(classifier,
-                        evaluation_threshold,
-                        dataset_filename):
-
-
-    if isinstance(dataset_filename, pandas.DataFrame):
-        dataset = dataset_filename.__copy__()
-        samples = len(dataset_filename.index)
-        negatives = dataset_filename[dataset_filename["Annots"] == 0].count()["Annots"]
-        positives = samples - negatives
-    else:
-        # read data
-        dataset, negatives, positives = preproc.read_data(dataset_filename)
-
-
-    annots = dataset["Annots"].tolist()
-
-    true_positives = 0
-    true_negatives = 0
-    false_positives = 0
-    false_negatives = 0
-
-    classifier_output = [0] * len(annots)
-
-    for rule in classifier.rule_set:  # evaluating every rule in the classifier
-        columns = []
-        for input in rule.pos_inputs:
-            columns.append(dataset[input].tolist())
-        for input in rule.neg_inputs:
-            columns.append([not x for x in dataset[input].tolist()])
-
-        rule_output = [1] * len(annots)
-
-        for column in columns:
-            rule_output = [i and j for i, j in zip(rule_output, column)]
-
-        classifier_output = [i + j for i, j in zip(classifier_output, rule_output)]
-
-    dec = Decimal(evaluation_threshold * len(classifier.rule_set)).to_integral_value(rounding=ROUND_HALF_UP)
-    outputs = []
-    for i in classifier_output:
-        if i >= dec:
-            outputs.append(1)
-        else:
-            outputs.append(0)
-
-    for i in range(0, len(annots)):
-        if annots[i] == 1 and outputs[i] == 1:
-            true_positives = true_positives + 1
-        if annots[i] == 0 and outputs[i] == 0:
-            true_negatives = true_negatives + 1
-        if annots[i] == 1 and outputs[i] == 0:
-            false_negatives = false_negatives + 1
-        if annots[i] == 0 and outputs[i] == 1:
-            false_positives = false_positives + 1
-
-    bacc = calculate_balanced_accuracy(true_positives, true_negatives, positives, negatives)
-
-    print("BACC: ", bacc)
-    print("TP: ", true_positives)
-    print("TN: ", true_negatives)
-    print("FP: ", false_positives)
-    print("FN: ", false_negatives)
-
-    return bacc
-
-
-def generate_parameters(tune_weights, bacc_lower, bacc_upper, bacc_step,
-                        iter_lower, iter_upper, iter_step,
-                        pop_lower, pop_upper, pop_step,
-                        cp_lower, cp_upper, cp_step,
-                        mp_lower, mp_upper, mp_step,
-                        ts_lower, ts_upper, ts_step,
-                        param_sets_numb):
-
-    if tune_weights:
-        bacc_weights = [i for i in range(bacc_lower, bacc_upper+1, bacc_step)]
-    iterations = [i for i in range(iter_lower, iter_upper+1, iter_step)]
-    population_size = [i for i in range(pop_lower, pop_upper+1, pop_step)]
-    crossover_probability = [i/100 for i in range(cp_lower, cp_upper+1, cp_step)]
-    mutation_probability = [i/100 for i in range(mp_lower, mp_upper+1, mp_step)]
-    tournament_size = [i/100 for i in range(ts_lower, ts_upper+1, ts_step)]
-
-    parameter_sets = []
-    for i in range(0, param_sets_numb):
-        if tune_weights:
-            weight = bacc_weights[random.randrange(0, len(bacc_weights))]/100
-        else:
-            weight = bacc_lower
-        iter = iterations[random.randrange(0, len(iterations))]
-        pop = population_size[random.randrange(0, len(population_size))]
-        cp = crossover_probability[random.randrange(0, len(crossover_probability))]
-        mp = mutation_probability[random.randrange(0, len(mutation_probability))]
-        ts = tournament_size[random.randrange(0, len(tournament_size))]
-        while [iter, pop, cp, mp, ts] in parameter_sets:
-            if tune_weights == True:
-                weight = bacc_weights[random.randrange(0, len(bacc_weights))]
-            iter = iterations[random.randrange(0, len(iterations))]
-            pop = population_size[random.randrange(0, len(population_size))]
-            cp = crossover_probability[random.randrange(0, len(crossover_probability))]
-            mp = mutation_probability[random.randrange(0, len(mutation_probability))]
-            ts = tournament_size[random.randrange(0, len(tournament_size))]
-        parameter_sets.append([weight, iter, pop, cp, mp, ts])
-
-    return parameter_sets
-
-
-def generate_bash_scripts(train_list, test_list):
-
-    parameter_sets = generate_parameters(25, 100, 25,
-                                         50, 300, 50,
-                                         10, 100, 10,
-                                         10, 100, 10,
-                                         10, 50, 10,
-                                         100)
-    run_commands = []
-
-    for train, test in zip(train_list, test_list):
-        for i in range(0, len(parameter_sets)):
-
-            log_name = "log_" + train.replace(".csv", "") + "_" + str(parameter_sets[i][0]) + "_" \
-                       + str(parameter_sets[i][1]) + "_" + str(parameter_sets[i][2]) + "_" \
-                       + str(parameter_sets[i][3]) + "_" + str(parameter_sets[i][4])
-
-            run_command = "python3 /home/mnowicka/run_GA.py" + \
-                          " --train ./data/" + train + " --test ./data/" + test + \
-                          " -i " + str(parameter_sets[i][0]) + " -p " + str(parameter_sets[i][1]) + \
-                          " -c " + str(5) + " -a " + str(0.5) + " -x " + str(parameter_sets[i][2]) + \
-                          " -m " + str(parameter_sets[i][3]) + " -t " + str(parameter_sets[i][4]) + \
-                          " > " + log_name
-
-            script = "#!/bin/bash\n#SBATCH -J " + log_name + "\n#SBATCH -D /data/scratch/mnowicka\n#SBATCH -o " \
-                     + log_name + ".%j.out\n#SBATCH -n 1\n#SBATCH --time=5-00:00:00\n" \
-                                  "#SBATCH --partition=big\n#SBATCH --mem=2000M\n#SBATCH --cpus-per-task=1\n\n" \
-                                  "source /home/mnowicka/venv/bin/activate\n" + run_command
-
-            script_name = "C:/Projects/DISCRETIZATION/SIMDATA/D2/bash_scripts/script_" + log_name + ".sh"
-            script_file = open(script_name, 'w')
-            script_file.write(script)
-            script_file.close()
-
-
-def print_classifier(classifier):
-
-    classifier_message = ""
-
-    for rule in classifier.rule_set:
-        rule_message = ""
-
-        if len(rule.neg_inputs) == 0 and len(rule.pos_inputs) == 1:
-            rule_message = "(" + str(rule.pos_inputs[0]) + ")"
-            # print(rule_message)
-        elif len(rule.pos_inputs) == 0 and len(rule.neg_inputs) == 1:
-            rule_message = "(NOT " + str(rule.neg_inputs[0]) + ")"
-            # print(rule_message)
-        else:
-            for input in rule.pos_inputs:
-                rule_message = rule_message + "(" + input + ")"
-                # print(rule_message)
-            for input in rule.neg_inputs:
-                rule_message = rule_message + "(NOT " + input + ")"
-                # print(rule_message)
-
-        rule_message = " [" + rule_message + "] "
-
-        rule_message = rule_message.replace(")(", ") AND (")
-
-        classifier_message = classifier_message + rule_message
-
-    return classifier_message
-
-
-
-
---
+++ tuner.py
@@ -0,0 +1,158 @@
+import random
+import numpy
+import eval
+from multiprocessing import Pool
+from functools import partial
+
+
+# generate parameters
+def generate_parameters(tune_weights, weight_lower, weight_upper, weight_step,
+                        iter_lower, iter_upper, iter_step,
+                        pop_lower, pop_upper, pop_step,
+                        cp_lower, cp_upper, cp_step,
+                        mp_lower, mp_upper, mp_step,
+                        ts_lower, ts_upper, ts_step,
+                        param_sets_numb):
+
+    parameter_sets = []  # list of param_sets_numb parameter sets
+    for i in range(0, param_sets_numb):
+        if tune_weights:  # if weight tuning is on
+            weight = random.randrange(weight_lower, weight_upper + 1, weight_step) / 100  # generate weight
+        else:
+            weight = weight_lower
+        iter = random.randrange(iter_lower, iter_upper+1, iter_step)  # iterations
+        pop = random.randrange(pop_lower, pop_upper+1, pop_step)  # population size
+        cp = random.randrange(cp_lower, cp_upper+1, cp_step) / 100  # crossover probability
+        mp = random.randrange(mp_lower, mp_upper+1, mp_step) / 100  # mutation probability
+        ts = random.randrange(ts_lower, ts_upper+1, ts_step) / 100  # tournament size
+        while [iter, pop, cp, mp, ts] in parameter_sets:  # if parameter set is already listed
+            if tune_weights:
+                weight = random.randrange(weight_lower, weight_upper + 1, weight_step) / 100
+            iter = random.randrange(iter_lower, iter_upper + 1, iter_step)
+            pop = random.randrange(pop_lower, pop_upper + 1, pop_step)
+            cp = random.randrange(cp_lower, cp_upper + 1, cp_step) / 100
+            mp = random.randrange(mp_lower, mp_upper + 1, mp_step) / 100
+            ts = random.randrange(ts_lower, ts_upper + 1, ts_step) / 100
+        parameter_sets.append([weight, iter, pop, cp, mp, ts])  # add set to list
+
+    return parameter_sets
+
+
+# parameter tuning
+def tune_parameters(training_cv_datasets, testing_cv_datasets, feature_cdds, config,
+                    classifier_size, evaluation_threshold, rule_list, test_repeats):
+
+    # get the parameters from configuration file
+    tune_weights = config.getboolean("PARAMETER TUNING", "TuneWeights")
+
+    if tune_weights:  # assign bounds if weight tuning is on
+        weight_lower_bound = int(config['PARAMETER TUNING']['WeightLowerBound'])
+        weight_upper_bound = int(config['PARAMETER TUNING']['WeightUpperBound'])
+        weight_step = int(config['PARAMETER TUNING']['WeightStep'])
+    else:  # otherwise assign weight
+        weight_lower_bound = float(config['OBJECTIVE FUNCTION']['Weight'])
+        weight_upper_bound = float(config['OBJECTIVE FUNCTION']['Weight'])
+        weight_step = float(config['OBJECTIVE FUNCTION']['Weight'])
+
+    # iteration bounds
+    iteration_lower_bound = int(config['PARAMETER TUNING']['IterationLowerBound'])
+    iteration_upper_bound = int(config['PARAMETER TUNING']['IterationUpperBound'])
+    iteration_step = int(config['PARAMETER TUNING']['IterationStep'])
+
+    # population size range
+    population_lower_bound = int(config['PARAMETER TUNING']['PopulationLowerBound'])
+    population_upper_bound = int(config['PARAMETER TUNING']['PopulationUpperBound'])
+    population_step = int(config['PARAMETER TUNING']['PopulationStep'])
+
+    # crossover probability bounds
+    crossover_lower_bound = int(config['PARAMETER TUNING']['CrossoverLowerBound'])
+    crossover_upper_bound = int(config['PARAMETER TUNING']['CrossoverUpperBound'])
+    crossover_step = int(config['PARAMETER TUNING']['CrossoverStep'])
+
+    # mutation probability bounds
+    mutation_lower_bound = int(config['PARAMETER TUNING']['MutationLowerBound'])
+    mutation_upper_bound = int(config['PARAMETER TUNING']['MutationUpperBound'])
+    mutation_step = int(config['PARAMETER TUNING']['MutationStep'])
+
+    # tournament size bounds
+    tournament_lower_bound = int(config['PARAMETER TUNING']['TournamentLowerBound'])
+    tournament_upper_bound = int(config['PARAMETER TUNING']['TournamentUpperBound'])
+    tournament_step = int(config['PARAMETER TUNING']['TournamentStep'])
+
+    # number of parameter sets
+    number_of_sets = int(config['PARAMETER TUNING']['NumberOfSets'])
+
+    # number of available processors to parallelize cross-validation
+    processes = int(config['PARALELIZATION']['ProccessorNumb'])
+
+    # generate parameter sets
+    parameter_sets = generate_parameters(tune_weights,
+                                         weight_lower_bound,
+                                         weight_upper_bound,
+                                         weight_step,
+                                         iteration_lower_bound,
+                                         iteration_upper_bound,
+                                         iteration_step,
+                                         population_lower_bound,
+                                         population_upper_bound,
+                                         population_step,
+                                         crossover_lower_bound,
+                                         crossover_upper_bound,
+                                         crossover_step,
+                                         mutation_lower_bound,
+                                         mutation_upper_bound,
+                                         mutation_step,
+                                         tournament_lower_bound,
+                                         tournament_upper_bound,
+                                         tournament_step,
+                                         number_of_sets)
+
+    # test parameter sets
+    best_parameter_set = parameter_sets[0]  # assign first set to best parameter set
+    best_avg_test_bacc = 0.0  # assign best bacc
+    best_avg_test_std = 1.0  # assign best std
+
+    parameter_set_number = 0  # parameter set counter
+
+    # iterate over parameter sets
+    for parameter_set in parameter_sets:
+
+        parameter_set_number += 1
+        print("\nTESTING PARAMETER SET ", parameter_set_number, ": ", parameter_set)
+
+        cv_datasets = zip(training_cv_datasets, testing_cv_datasets, feature_cdds)
+
+        # train and test on each fold
+        if processes > 1:  # if more than one processor is available
+            with Pool(processes) as p:
+                cv_results = p.map(partial(train_and_test, parameter_set=parameter_set, classifier_size=classifier_size,
+                                           evaluation_threshold=evaluation_threshold, rule_list=rule_list,
+                                           repeats=test_repeats, print_results=False), cv_datasets)
+        else:
+            cv_results = list(map(partial(train_and_test, parameter_set=parameter_set, classifier_size=classifier_size,
+                                          evaluation_threshold=evaluation_threshold, rule_list=rule_list,
+                                          repeats=test_repeats, print_results=False), cv_datasets))
+
+        # unpack values
+        test_bacc_cv, test_std_cv = zip(*cv_results)
+
+        # calculate average bacc scores for folds and std
+        test_bacc_avg = numpy.average(test_bacc_cv)
+        test_std_avg = numpy.std(test_bacc_avg)
+
+        print("\nRESULTS PARAMETER SET ", parameter_set_number, ": ", parameter_set)
+        print("TEST AVG BACC: ", test_bacc_avg, ", STD: ", test_std_avg)
+
+        # check for improvement
+        if eval.is_higher(best_avg_test_bacc, test_bacc_avg):
+            best_parameter_set = parameter_set
+            best_avg_test_bacc = test_bacc_avg
+            best_avg_test_std = test_std_avg
+
+        # if new score is not higher but std is lower assign new parameter set as best
+        elif eval.is_close(best_avg_test_bacc, test_bacc_avg) and eval.is_higher(best_avg_test_std, test_std_avg):
+            best_parameter_set = parameter_set
+            best_avg_test_bacc = test_bacc_avg
+            best_avg_test_std = test_std_avg
+
+    return best_parameter_set, best_avg_test_bacc, best_avg_test_std
